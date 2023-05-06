3ef0a9cfb067ec0057bcf98f85fad91783a39410
izuna_aja <116055126+izunalagi@users.noreply.github.com>
2023-05-07 06:38:40 +0700

    Update index.blade.php

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -10,7 +10,7 @@
 
                     <br>
 
-                    <div>
+                    <div class="card-body">
 
                         <a href="{{ route('transaction.create') }}">
                             <button type="button" class="btn btn-outline-success">Buat</button>
@@ -21,57 +21,57 @@
                         </a>
                         <br>
                         <br>
-                    </div>
 
 
-                    <table class="table table-bordered">
-                        <thead>
+                        <table class="table table-bordered">
+                            <thead>
 
-                            <tr>
-                                <th>No</th>
-                                <th>buyer</th>
-                                <th>date</th>
-                                <th>transaksi</th>
-                                <th>Status</th>
-                                <th>Action</th>
-
-                            </tr>
-
-                        </thead>
-                        </th>
-                        <tbody>
-                            @foreach ($transactions as $item)
                                 <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $item->fkBuyer->name }}</td>
-                                    <td>{{ $item->date }}</td>
-                                    <td><a href="{{ route('checkout.index', $item->id) }}">
-                                            <button type="button" class="btn btn-info">Detail Transaksi</button>
-                                        </a></td>
-                                    <td>
-                                        <div class="d-grid gap-2 col-6 mx-auto">
+                                    <th>No</th>
+                                    <th>buyer</th>
+                                    <th>date</th>
+                                    <th>transaksi</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
 
-                                            @if ($item->status == '0')
-                                                <button class="btn btn-warning" type="button">Menunggu</button>
-                                            @else
-                                                <button type="button" class="btn btn-success" disabled>Selesai</button>
-                                            @endif
-
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <form action="{{ route('transaction.destroy', $item->id) }}" method="POST">
-                                            <a type="button" class="btn btn-secondary"
-                                                href="{{ route('transaction.edit', $item->id) }}">Edit</a>
-                                            @method('delete')
-                                            @csrf
-                                            <button type="submit" class="btn btn-danger">Delete</button>
-                                        </form>
-                                    </td>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+
+                            </thead>
+                            </th>
+                            <tbody>
+                                @foreach ($transactions as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->fkBuyer->name }}</td>
+                                        <td>{{ $item->date }}</td>
+                                        <td><a href="{{ route('checkout.index', $item->id) }}">
+                                                <button type="button" class="btn btn-info">Detail Transaksi</button>
+                                            </a></td>
+                                        <td>
+                                            <div class="d-grid gap-2 col-6 mx-auto">
+
+                                                @if ($item->status == '0')
+                                                    <button class="btn btn-warning" type="button">Menunggu</button>
+                                                @else
+                                                    <button type="button" class="btn btn-success" disabled>Selesai</button>
+                                                @endif
+
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <form action="{{ route('transaction.destroy', $item->id) }}" method="POST">
+                                                <a type="button" class="btn btn-secondary"
+                                                    href="{{ route('transaction.edit', $item->id) }}">Edit</a>
+                                                @method('delete')
+                                                @csrf
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
 
 
                     </tr>
